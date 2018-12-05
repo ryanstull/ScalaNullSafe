381c0554fb4023e171e0a10c608c00da6da31a4c
ryanstull <rstull1200@gmail.com>
2018-12-05 02:43:24 -0500

    More tests

--- src/test/scala/com/ryanstull/nullsafe/Tests.scala
+++ src/test/scala/com/ryanstull/nullsafe/Tests.scala
@@ -9,10 +9,16 @@ import org.scalatest.FlatSpec
 class Tests extends FlatSpec {
 
 	case class E(s: String)
-	case class D(e: E)
+	case class D(e: E){
+		def getE: E = E(null)
+	}
 	case class C(d: D)
-	case class B(c: C)
-	case class A(b: B)
+	case class B(c: C){
+		def getEmptyC: C = C(null)
+	}
+	case class A(b: B){
+		def getAB(string: String) = B(null)
+	}
 
 	"Simple field access" should "not cause an NPE" in {
 
@@ -25,5 +31,29 @@ class Tests extends FlatSpec {
 		val a = A(B(C(null)))
 
 		?(a.b.c.d.e.s)
+	}
+
+	"No-op method calls" should "not cause an NPE" in {
+		val b = null.asInstanceOf[B]
+
+		?(b.getEmptyC)
+	}
+
+	"Mixing field access and no-op method calls" should "not cause an NPE" in {
+		val a = A(null)
+
+		?(a.b.getEmptyC.d.e)
+	}
+
+	"Function calls with args" should "not cause an NPE" in {
+		val a = null.asInstanceOf[A]
+
+		?(a.getAB(""))
+	}
+
+	"Mixing field access, various arities of function calls" should "not cause an NPE" in {
+		val a = null.asInstanceOf[A]
+
+		?(a.getAB("").getEmptyC.d.e.s)
 	}
 }
