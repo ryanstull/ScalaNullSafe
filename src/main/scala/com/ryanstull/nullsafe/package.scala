0d18296c33536644f1226a7b21f3adb0b5e224a0
ryanstull <rstull1200@gmail.com>
2019-01-26 17:07:02 -0500

    WIP

--- src/main/scala/com/ryanstull/nullsafe/package.scala
+++ src/main/scala/com/ryanstull/nullsafe/package.scala
@@ -30,17 +30,17 @@ package object nullsafe {
 		import c.universe._
 		import mutable.{Queue => MQueue}
 
-		def decomposeExp(tree: Tree): (Tree , MQueue[Tree => Tree]) = {
-			def loop(tree: Tree, accumulator: (Tree, MQueue[Tree => Tree]) = (null,MQueue.empty[Tree => Tree])): (Tree, MQueue[Tree => Tree]) = {
-				tree match {
-					case t @ (_:Ident | _:This | Apply(Select(New(_), _), _)) => (t, MQueue.empty[Tree => Tree])
-					case Select(qualifier, predName) =>
+		def decomposeExp(tree: Tree): (Tree , MQueue[(Tree => Tree,Tree)]) = {
+			def loop(tree: Tree, accumulator: (Tree, MQueue[(Tree => Tree, Tree)]) = (null,MQueue.empty)): (Tree, MQueue[(Tree => Tree, Tree)]) = {
+				tree match {				   //↓ This case captures constructor calls
+					case t @ (_:Ident | _:This | Apply(Select(New(_), _), _)) => (t, MQueue.empty)
+					case t @ Select(qualifier, predName) =>
 						val res = loop(qualifier, accumulator)
-						res._2 += ((qual: c.universe.Tree) => Select(qual,predName))
+						res._2 += ((qual: c.universe.Tree) => Select(qual,predName)) -> t
 						res
-					case Apply(Select(qualifier, predicate), args) =>
+					case t @ Apply(Select(qualifier, predicate), args) =>
 						val res = loop(qualifier, accumulator)
-						res._2 += ((qual: c.universe.Tree) => Apply(Select(qual, predicate), args))
+						res._2 += ((qual: c.universe.Tree) => Apply(Select(qual, predicate), args)) -> t
 						res
 					case _ => throw new IllegalArgumentException
 				}
@@ -48,39 +48,65 @@ package object nullsafe {
 			loop(tree)
 		}
 
-		def buildIfGuarded(prefix: Tree,selects: MQueue[Tree => Tree]): Tree = {
+		def buildIfGuardedExp(prefix: Tree,selects: MQueue[(Tree => Tree, Tree)]): Tree = {
 
-			def ifStatement(tree: Tree): Tree = reify {
-				if (c.Expr(tree).splice != null) {
-					c.Expr(checkForLastSelect(tree)).splice
-				} else null
-			}.tree
+			def advanceToNextNullable(tree: Tree,first: Boolean): Tree = {
 
-			def checkForLastSelect(tree: Tree): Tree = {
-				if (selects.size == 1) {
-					selects.dequeue().apply(tree)
+				def canBeNull(sub: Tree): Boolean = {
+					val sym = sub.symbol
+					val tpe = sub.tpe
+
+					sym != null &&
+						!sym.isModule && !sym.isModuleClass &&
+						!sym.isPackage && !sym.isPackageClass &&
+						!(tpe <:< typeOf[AnyVal])
+				}
+
+				if(first) {
+					tree
 				} else {
-					newVal(tree, selects.dequeue())
+					var origTree: Tree = null
+					var newTree = tree
+					do {
+						val next = selects.dequeue()
+						origTree = next._2
+						newTree = next._1.apply(newTree)
+					} while (selects.nonEmpty && !canBeNull(origTree))
+					newTree
 				}
 			}
 
-			def newVal(tree: Tree, processRhs: Tree => Tree): Tree = {
+			def newVal(prefix: Tree): Tree = {
 				val baseTermName = TermName(c.freshName)
 				val ident = Ident(baseTermName)
 
 				Block(
-					ValDef(Modifiers(), baseTermName, TypeTree(), processRhs(tree)),
+					ValDef(Modifiers(Flag.SYNTHETIC), baseTermName, TypeTree(prefix.tpe), prefix),
 					ifStatement(ident)
 				)
 			}
 
-			if (prefix.symbol.isMethod) {
-				newVal(prefix,f => f)
-			} else if(selects.isEmpty) prefix
-			else ifStatement(prefix)
+			def ifStatement(prefix: Tree): Tree = reify {
+				if (c.Expr(prefix).splice != null) {
+					c.Expr(next(prefix,first = false)).splice
+				} else null
+			}.tree
+
+			def next(tree: Tree,first: Boolean): Tree = {
+				val newTree = advanceToNextNullable(tree,first)
+
+				if (selects.isEmpty) newTree
+				else if (first) {
+					ifStatement(newTree)
+				} else {
+					newVal(newTree)
+				}
+			}
+
+			next(prefix, first = true)
 		}
 
 		val (baseTerm, selections) = decomposeExp(tree)
-		buildIfGuarded(baseTerm,selections)
+		buildIfGuardedExp(baseTerm,selections)
 	}
 }
---
+++ src/test/scala/com/ryanstull/nullsafe/HasStaticMethod.java
@@ -0,0 +1,13 @@
+package com.ryanstull.nullsafe;
+
+/**
+ * @author ryan
+ * @since 1/12/19.
+ */
+public class HasStaticMethod {
+
+	public static String staticGetString() {
+		return "";
+	}
+
+}
